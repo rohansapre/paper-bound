9540a9190d86e85f09eb4f9b597f131eee55a707
rohansapre <11bit025@nirmauni.ac.in>
2016-04-17 16:47:06 +0530

    Finished the fixed pages

--- test/controllers/fixed_pages_controller_test.rb
+++ test/controllers/fixed_pages_controller_test.rb
@@ -4,16 +4,19 @@ class FixedPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | PaperBound"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | PaperBound"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | PaperBound"
   end
 
 end
