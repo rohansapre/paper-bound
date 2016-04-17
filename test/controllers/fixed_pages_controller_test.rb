f7f34d1476f67d7ff0fc47928520f7e62d033ee5
rohansapre <11bit025@nirmauni.ac.in>
2016-04-17 17:01:36 +0530

    Defined a page title helper

--- test/controllers/fixed_pages_controller_test.rb
+++ test/controllers/fixed_pages_controller_test.rb
@@ -4,7 +4,7 @@ class FixedPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | PaperBound"
+    assert_select "title", "PaperBound"
   end
 
   test "should get help" do
